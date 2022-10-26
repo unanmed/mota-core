ca5b6dbacd51407057db7bcb8de3f64b5aadf9b1
unanmed <1319491857@qq.com>
2022-10-26 14:10:15 +0800

    init

---
+++ src/types/core.d.ts
@@ -0,0 +1,11 @@
+export interface MotaCore {
+    _WIDTH_: number;
+    _HEIGHT_: number;
+    _PX_: number;
+    _PY_: number;
+    _HALF_WIDTH_: number;
+    _HALF_HEIGHT_: number;
+    __SIZE__: number;
+    __PIXELS__: number;
+    __HALF_SIZE__: number;
+}
